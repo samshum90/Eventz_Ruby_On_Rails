3e9c6222dce8c1b6abd347796cc7ffeba5bc8e17
Sam <samshum90@gmail.com>
2021-04-27 15:48:37 +0100

    completed section 14

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   
   root "events#index"
 
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
+  resources :events
+  # get "events" => "events#index"
+  # get "events/new" => "events#new"
+  # get "events/:id" => "events#show", as: "event"
+  # get "events/:id/edit" => "events#edit", as: "edit_event"
+  # patch "events/:id" => "events#update"
 end
