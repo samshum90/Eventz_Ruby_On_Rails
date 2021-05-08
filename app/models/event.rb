c8d275ca96e6ab748b6d6d75a5c4cec8eb2103c3
Sam <samshum90@gmail.com>
2021-05-08 20:43:54 +0100

    finished section 29

--- app/models/event.rb
+++ app/models/event.rb
@@ -22,4 +22,8 @@ class Event < ApplicationRecord
     def free?
         price.blank? || price.zero?
     end
+
+    def sold_out?
+        (capacity - registrations.size).zero?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   
+  
   resources :registrations
   root "events#index"
 
   resources :events do 
     resources :registrations
   end
+
+  resources :users
+  get "signup" => "users#new"
   # get "events" => "events#index"
   # get "events/new" => "events#new"
   # get "events/:id" => "events#show", as: "event"
